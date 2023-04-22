ec3272b30a8f9dc3047782471ed87e0b5cd212c5
BeaS84 <131193427+BeaS84@users.noreply.github.com>
2023-04-22 14:15:45 +0200

    Exercise 1 resolved

--- zajecia2204Ola/src/main/java/org/example/App.java
+++ zajecia2204Ola/src/main/java/org/example/App.java
@@ -39,18 +39,72 @@ public class App
              }
          }
         for (Integer inter: interjection){
-            System.out.println(inter);
+            System.out.println("Interjestion: "+ inter);
         }
 
 
 
 
 
-//        thirdSet.addAll(secondSet);
+        thirdSet.addAll(secondSet);
+
+         for (Integer number: thirdSet){
+             System.out.println(number);
+         }
+
+       // suma
+
+        System.out.println("Suma:");
+
+        Set<Integer> sumWithCommonPart = new HashSet<>();
+        sumWithCommonPart.addAll(firstSet);
+        sumWithCommonPart.addAll(secondSet);
+
+
+        for (Integer number : sumWithCommonPart){
+            System.out.println(number);
+        }
+
+
+        // suma bez czesci wspolnej to Suma - czesc wspolna
+        //sumWithCommonPart - interjection 0,4,6,8,9
+        //sumWithCommonPar 0,1,4,6,8,9,12
+        //interjection 1, 12
+
+
+//        Set<Integer> sumWithoutCommonPart = new HashSet<>();
 //
-//         for (Integer number: thirdSet){
-//             System.out.println(number);
-//         }
+//        for( Integer num: sumWithCommonPart){
+//            for(Integer num2: interjection){
+//                if(num !=num2){
+//                    sumWithoutCommonPart.add(num2);
+//
+//                   // sumWithoutCommonPart.add(num);
+//                }
+//            }
+//        }
+//        for(Integer el: sumWithoutCommonPart){
+//            System.out.println("to jest set bez czesci wspolnej: " + el);
+//        }
+
+
+
+
+
+
+        // difference between set first and second - 4,6,8
+
+        Set<Integer> difference = new HashSet<>(firstSet);
+        difference.removeAll(secondSet);
+
+        for(Integer num : difference){
+            System.out.println(num);
+        }
+
+
+
+
+
 
 
 
